8d11684aed1c20119b4270f0a58683f4dfcb87f9
Ritik <ritik.k.1404@gmail.com>
2024-09-30 10:18:23 +0530

    Initial Commit

---
+++ FireLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FireLog
+//
+//  Created by Ritik Karma on 30/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FireLog/FireLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireLogApp.swift
+//  FireLog
+//
+//  Created by Ritik Karma on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FireLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
