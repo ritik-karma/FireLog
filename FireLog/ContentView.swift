56e88c7df50117e3aa11d0e5d753dc2b201f31ab
Ritik <ritik.k.1404@gmail.com>
2024-09-30 18:37:26 +0530

    notes

--- FireLog/ContentView.swift
+++ FireLog/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "bulb")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            //MARK: Make this button code in Component❗️
+            Button{
+                //Button Logic
+            } label: {
+                Text("Login")
+                    .frame(width: 350, height: 50)
+                    .background(.purple)
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+            }
+            
+            HStack{
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(Color.gray)
+                Text("Or")
+                    .font(.headline)
+                    .foregroundColor(Color.gray)
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundStyle(Color.gray)
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
