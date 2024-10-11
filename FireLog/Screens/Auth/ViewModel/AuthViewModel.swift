67d8c8f530d8966ac1cc708167dafca50ecfc6d0
Ritik <ritik.k.1404@gmail.com>
2024-10-11 11:36:21 +0530

    minor changes

--- FireLog/Screens/Auth/ViewModel/AuthViewModel.swift
+++ FireLog/Screens/Auth/ViewModel/AuthViewModel.swift
@@ -10,6 +10,9 @@ import FirebaseAuth
 import FirebaseFirestore
 
 final class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?  //Firebase user
+    @Published var currentUser: User?   //Created by us user
+    @Published var isError: Bool = false
     private let auth = Auth.auth()
     private let firestore = Firestore.firestore()
     
